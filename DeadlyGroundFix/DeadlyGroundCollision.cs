34826d5250b85a8d2a810733f5823c400ac44c50
Etherpod <etherpod@gmail.com>
2024-06-06 13:30:26 -0700

    Initial commit

---
+++ DeadlyGroundFix/DeadlyGroundCollision.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace DeadlyGroundFix;
+
+public class DeadlyGroundCollision : MonoBehaviour
+{
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        GetComponent<DeathZone>().OnTriggerExit2D(other.collider);
+    }
+}
---
+++ DeadlyGroundFix/ModMain.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using modweaver.core;
+
+namespace DeadlyGroundFix;
+
+[ModMainClass]
+public class ModMain : Mod
+{
+    public override void Init()
+    {
+        Harmony harmony = new Harmony(Metadata.id);
+        harmony.PatchAll();
+    }
+
+    public override void Ready() { }
+
+    public override void OnGUI(ModsMenuPopup ui) { }
+}
+
+[HarmonyPatch]
+public class PatchClass
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(DeathZone), nameof(DeathZone.Start))]
+    public static void DeadlyGroundFix(DeathZone __instance)
+    {
+        if (!__instance._collider2D.isTrigger)
+        {
+            __instance.gameObject.AddComponent<DeadlyGroundCollision>();
+        }
+    }
+}
